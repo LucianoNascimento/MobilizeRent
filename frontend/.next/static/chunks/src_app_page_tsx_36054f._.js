2d6538256c9842821532c9ab3549d5b8a6ebe89d
LucianoNascimento <lucianosantosti@hotmail.com>
2025-02-15 22:21:59 -0300

    indexpage

--- frontend/.next/static/chunks/src_app_page_tsx_36054f._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_36054f._.js
@@ -5,8 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_36054f._.js",
   "chunks": [
-    "static/chunks/node_modules_next_dist_ea69ac._.js",
-    "static/chunks/src_app_page_tsx_8b213b._.js"
+    "static/chunks/_6046eb._.js"
   ],
   "source": "dynamic"
 });
